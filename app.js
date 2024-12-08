b1b41a23a4dda5ecc04cc622d355a5a1f49bb20e
Mohammed Almethen <almethen2002@gmail.com>
2024-12-08 12:54:31 +0300

    new app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const accounts = require("./accounts");
 const app = express();
 const accountRoutes = require("./apis/accounts/routes");
+const connectDB = require("./database");
 app.use(express.json()); // solves req.body undefined!
 app.use("/api/accounts", accountRoutes);
 
+connectDB();
 app.listen(8000, () => {
   console.log("Hello this is my first server :)");
 });

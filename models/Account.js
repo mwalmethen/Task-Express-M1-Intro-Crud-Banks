64b61f8a1bfa3c575d751760f8f5ef4c12bd2dd1
Mohammed Almethen <almethen2002@gmail.com>
2024-12-08 12:53:05 +0300

    updated model file

--- models/Account.js
+++ models/Account.js
@@ -1,8 +1,15 @@
 const { model, Schema } = require("mongoose");
 
 const AccountSchema = new Schema({
-  username: String,
-  funds: Number,
+  username: {
+    type: String, // type of the object
+    required: true, // return true but username is required you cant create an account without username
+    default: null, // makes the value null
+  },
+  funds: {
+    type: Number, // type of object
+    default: 0, // default value should be zero
+  },
 });
 
 module.exports = model("Account", AccountSchema);
